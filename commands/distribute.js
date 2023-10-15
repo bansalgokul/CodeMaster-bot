dd896571f23db86721efed5c5b5956368d2dc4f9
Navansh <khandelwalnavansh1@gmail.com>
2023-10-15 15:15:24 +0530

    Update distribute.js

--- commands/distribute.js
+++ commands/distribute.js
@@ -1,53 +1,90 @@
 const { SlashCommandBuilder } = require("discord.js");
 
-// Replace this with a function to distribute rewards
-async function distributeRewardsToEligibleParticipants(entries) {
-	// Implement your logic to distribute rewards here
-	// This can involve interacting with a database or external APIs
-	// For demonstration purposes, we'll just simulate distributing rewards.
-	return entries.map((entry) => ({
-		user: entry.user,
-		reward: "Token", // Replace with the actual reward
-	}));
+const axios = require('axios');
+
+// Replace this with your actual function to distribute scaler coins
+async function distributeScalerCoinsToUser(user, amount) {
+    try {
+        // Replace with your actual API endpoint and authentication
+        const apiEndpoint = 'https://api.scalercoinservice.com/distribute';
+        const apiKey = 'your_api_key_here';
+
+        // Prepare the data payload according to your API's specification
+        const payload = {
+            user: user, // assuming 'user' is sufficient for identification; you might need more info
+            amount: amount
+        };
+
+        // Make the POST request to the external API
+        const response = await axios.post(apiEndpoint, payload, {
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // Handle the response according to your needs
+        if (response.status === 200) {
+            return {
+                success: true,
+                data: response.data // or whatever part of the response you need
+            };
+        } else {
+            // Handle non-success status codes as needed
+            return {
+                success: false,
+                message: 'Failed to distribute scaler coins due to a non-success status code.'
+            };
+        }
+    } catch (error) {
+        // Handle errors as needed (e.g., network errors, API errors)
+        console.error('Error distributing scaler coins:', error);
+        return {
+            success: false,
+            message: error.message || 'Unexpected error occurred.'
+        };
+    }
 }
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("distribute")
-		.setDescription(
-			"Distribute tokens or rewards to eligible participants."
-		),
-	async execute(interaction) {
-		// Assume you have a list of entries with eligibility criteria
-		const entries = [
-			{ user: "User1", eligible: true, content: "Entry 1" },
-			{ user: "User2", eligible: true, content: "Entry 2" },
-			{ user: "User3", eligible: false, content: "Entry 3" },
-			// Add more entries here...
-		];
+    data: new SlashCommandBuilder()
+        .setName('distribute')
+        .setDescription('Distribute scaler coins to eligible participants.'),
+    async execute(interaction) {
+        // Sample entries; in production, these should come from your data source
+        const entries = [
+            { user: 'User1', eligible: true, content: 'Entry 1' },
+            { user: 'User2', eligible: true, content: 'Entry 2' },
+            { user: 'User3', eligible: false, content: 'Entry 3' },
+            // ... more entries
+        ];
 
-		// Filter entries to get eligible participants
-		const eligibleParticipants = entries.filter((entry) => entry.eligible);
+        // Filter for eligible participants
+        const eligibleParticipants = entries.filter(entry => entry.eligible);
 
-		// Distribute rewards to eligible participants
-		const rewards = await distributeRewardsToEligibleParticipants(
-			eligibleParticipants
-		);
+        // Check if there are eligible participants
+        if (eligibleParticipants.length === 0) {
+            await interaction.reply('No eligible participants found for rewards.');
+            return;
+        }
 
-		if (rewards.length === 0) {
-			await interaction.reply(
-				"No eligible participants found to distribute rewards."
-			);
-		} else {
-			// Create a message with distributed rewards
-			const message = rewards
-				.map((reward) => {
-					return `Distributed ${reward.reward} to ${reward.user}`;
-				})
-				.join("\n");
+        // Distribute rewards and construct response message
+        let responseMessage = 'Rewards distributed:\n';
 
-			// Reply to the user with the distributed rewards
-			await interaction.reply(`Rewards distributed:\n${message}`);
-		}
-	},
+        for (const participant of eligibleParticipants) {
+            const amount = 100; // Define the amount or logic to calculate the amount of scaler coins
+            const result = await distributeScalerCoinsToUser(participant.user, amount);
+
+            if (result.success) {
+                responseMessage += `Distributed ${amount} Scaler Coins to ${participant.user}\n`;
+            } else {
+                // Log the error and inform the user of the issue
+                console.error(`Failed to distribute scaler coins to ${participant.user}: ${result.message}`);
+                responseMessage += `Failed to distribute rewards to ${participant.user}\n`;
+            }
+        }
+
+        // Send the result back in the channel
+        await interaction.reply(responseMessage);
+    },
 };
